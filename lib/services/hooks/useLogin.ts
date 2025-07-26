3e08f989840c37d6926ac3e9e421c6e10988172c
ghaith sheibani <154800068+ghaithsheibani@users.noreply.github.com>
2025-07-26 11:25:25 +0300

    fix login

--- lib/services/hooks/useLogin.ts
+++ lib/services/hooks/useLogin.ts
@@ -4,34 +4,38 @@ import axiosInstance from "../ApiCore";
 import { useAuthStore } from "@/lib/stores/authStore";
 
 const useLogin = () => {
-  const { setIsAuth, setUser } = useAuthStore();
+  const { setIsAuth, setUser, isAuth } = useAuthStore();
   return useMutation({
     mutationKey: ["Login"],
     mutationFn: (AdminLogin: LoginModel) => {
       const { email, password, rememberMe } = AdminLogin;
       return axiosInstance
-        .post(
-          "/login",
-          {
-            email: email,
-            password: password,
-          },
-          { withCredentials: true }
-        )
+        .post("/login", {
+          email: email,
+          password: password,
+        })
         .then((res) => {
-          const token = res.data.data.token;
-          console.log("Login response:", res.data.data);
+          const token = res.data.token;
+          console.log("Login response:", res.data);
           // Store user and partner in Zustand authStore (which will persist it)
-          setIsAuth(true);
-          setUser(res.data.data.user);
+          setUser(res.data.user);
           // Store token for axios request authorization header
+          setIsAuth(true);
+          console.log(isAuth);
           const storage = rememberMe ? localStorage : sessionStorage;
           storage.setItem("token", token);
 
           return storage;
         })
         .catch((error) => {
-          throw error.response.data;
+          // Handle different types of errors
+          if (error.response && error.response.data) {
+            throw error.response.data;
+          } else if (error.message) {
+            throw { message: error.message };
+          } else {
+            throw { message: "An unexpected error occurred" };
+          }
         });
     },
     onSuccess: () => {},
